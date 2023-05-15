88dbdef079eae28e27a29d92dab5aa596aedca45
Kamil <kamil@klemczak.pl>
2023-05-15 23:51:21 +0200

    Form done. Next step : connection to database

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -29,7 +29,7 @@ public class ListView extends VerticalLayout {
         configureForm();
 
         add(
-            getToolbar(),ś
+            getToolbar(),
             getContent()
         );
     }
